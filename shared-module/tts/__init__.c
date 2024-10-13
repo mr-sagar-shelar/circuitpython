1f89d2da1da000a4e2d24d8f8e55d69f9da31540
Sagar Shelar <sagarshelar@Sagars-MacBook-Pro.local>
2024-10-13 15:02:36 +0530

    Added flite method references

--- shared-module/tts/__init__.c
+++ shared-module/tts/__init__.c
@@ -4,8 +4,16 @@
 #include "py/runtime.h"
 #include "shared-bindings/os/__init__.h"
 #include "shared-bindings/tts/__init__.h"
+#include "flite.h"
+
+cst_voice *register_cmu_us_kal(const char *voxdir);
+void usenglish_init(cst_voice *v);
+void cmu_indic_lang_init(cst_voice *v);
+cst_lexicon *cmulex_init(void);
+cst_lexicon *cmu_indic_lex_init(void);
 
 mp_int_t shared_modules_tts_initialize() {
+    flite_init();
     return 0;
 }
 
@@ -14,6 +22,10 @@ mp_int_t shared_modules_tts_register_voice() {
 }
 
 mp_int_t shared_modules_tts_register_cmu_us_kal() {
+    cst_voice *v;
+    // v = register_cmu_us_kal(NULL);
+    // flite_add_lang("eng", usenglish_init, cmulex_init);
+    // flite_add_lang("usenglish", usenglish_init, cmulex_init);
     return 2;
 }
 
@@ -24,3 +36,14 @@ mp_int_t shared_modules_tts_synth_text() {
 mp_int_t shared_modules_tts_text_to_wave() {
     return 4;
 }
+
+/*
+Memory region         Used Size  Region Size  %age Used
+        READONLY:      964161 B      1536 KB     61.30%
+             RAM:       31496 B      1022 MB      0.00%
+
+
+Memory region         Used Size  Region Size  %age Used
+        READONLY:     3146480 B      1536 KB    200.05%
+            RAM:       35880 B      1022 MB      0.00%
+*/
