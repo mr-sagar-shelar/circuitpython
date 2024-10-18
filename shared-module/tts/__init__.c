2254e61a9fb5399596d7ebda20a5c8e5a5c6cd5c
Sagar Shelar <sagarshelar@Sagars-MacBook-Pro.local>
2024-10-18 08:51:06 +0530

    Added tts example code

--- shared-module/tts/__init__.c
+++ shared-module/tts/__init__.c
@@ -12,8 +12,34 @@ void cmu_indic_lang_init(cst_voice *v);
 cst_lexicon *cmulex_init(void);
 cst_lexicon *cmu_indic_lex_init(void);
 
+int i2s_stream_chunk(const cst_wave *w, int start, int size,
+                     int last, cst_audio_streaming_info *asi)
+{
+    // If you start to see multiple $ on console then it means TTS integration is working
+    printf(" $ ");
+    return CST_AUDIO_STREAM_CONT;
+}
+
 mp_int_t shared_modules_tts_initialize() {
     flite_init();
+
+    cst_voice *v;
+    v = register_cmu_us_kal(NULL);
+
+    flite_add_lang("eng", usenglish_init, cmulex_init);
+    flite_add_lang("usenglish", usenglish_init, cmulex_init);
+
+    cst_audio_streaming_info *asi = cst_alloc(struct cst_audio_streaming_info_struct, 1);
+    asi->min_buffsize = 256;
+    asi->asc = i2s_stream_chunk;
+    asi->userdata = NULL;
+    feat_set(v->features, "streaming_info", audio_streaming_info_val(asi));
+
+    cst_utterance *u = flite_synth_text("hello world, how are you doing today", v);
+    printf("\n Done TTS...\n");
+    delete_utterance(u);
+    u = NULL;
+
     return 0;
 }
 
@@ -22,7 +48,7 @@ mp_int_t shared_modules_tts_register_voice() {
 }
 
 mp_int_t shared_modules_tts_register_cmu_us_kal() {
-    cst_voice *v;
+    // cst_voice *v;
     // v = register_cmu_us_kal(NULL);
     // flite_add_lang("eng", usenglish_init, cmulex_init);
     // flite_add_lang("usenglish", usenglish_init, cmulex_init);
@@ -46,4 +72,15 @@ Memory region         Used Size  Region Size  %age Used
 Memory region         Used Size  Region Size  %age Used
         READONLY:     3146480 B      1536 KB    200.05%
             RAM:       35880 B      1022 MB      0.00%
+
+READONLY (rx): ORIGIN = 0x100000, LENGTH = 1536K --- mulitply by 3: 4608
+    RAM (rw): ORIGIN = 0x200000, LENGTH = 1022M ---- Add 3 * 1024
+
+0x100000 hex = 1048576 dec = 1024 kb
+0x200000 hex = 2097152 dec = 2048 kb + add difference of 3072 for 3 times 1024
+    = 2048 + 3072
+    = 5120
+    = 5120 * 1024
+    = 5242880 in decimal
+    =  0x500000 in hex
 */
